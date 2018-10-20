16effe40842412ebdfa82ad5a1cecbe4b0be199b
Kuba Mikita <jakub@underdev.it>
2018-10-20 14:34:39 +0200

    dsnap command help

--- index.js
+++ index.js
@@ -3,22 +3,23 @@
 const usage = require( 'command-line-usage' );
 
 const sections = [
-  {
-    header: 'underdev',
-    content: 'Set of usable aliases and scripts for WordPress development on top on Docker.'
-  },
-  {
-    header: 'Command list',
-    content: [
-      { name: 'dpr', summary: 'Creates new project.' },
-      { name: 'dup', summary: 'Starts docker-compose.' },
-      { name: 'dupd', summary: 'Starts docker-compose in background.' },
-      { name: 'ds', summary: 'Stops docker-compose.' },
-      { name: 'drm', summary: 'Removes docker-compose containers.' },
-      { name: 'dwp', summary: 'Executes the wp-cli command inside wordpress container. Usage: dwp core version' },
-      { name: 'dset', summary: 'Sets up WordPress - cleans default content and unused plugins and themes, sets up permalinks' },
-    ]
-  }
+	{
+		header: 'underdev',
+		content: 'Set of usable aliases and scripts for WordPress development on top on Docker.'
+	},
+	{
+		header: 'Command list',
+		content: [
+			{ name: 'dpr', summary: 'Creates new project.' },
+			{ name: 'dup', summary: 'Starts docker-compose.' },
+			{ name: 'dupd', summary: 'Starts docker-compose in background.' },
+			{ name: 'ds', summary: 'Stops docker-compose.' },
+			{ name: 'drm', summary: 'Removes docker-compose containers.' },
+			{ name: 'dwp', summary: 'Executes the wp-cli command inside wordpress container. Usage: dwp core version' },
+			{ name: 'dset', summary: 'Sets up WordPress - cleans default content and unused plugins and themes, sets up permalinks' },
+			{ name: 'dsnap', summary: 'Handles WP Snapshots. You can use without explicit configuration, ie: dsnap push' },
+		]
+	}
 ];
 
 console.log( usage( sections ) );
